1f6b49df422e16971fb6a04b47ec244aade3464f
jiruifeng <jiruifeng@jd.com>
2020-04-01 14:51:26 +0800

    flink sql demo

---
+++ flink-demo-sql/src/main/java/conf/sConf.java
@@ -0,0 +1,12 @@
+package conf;
+
+/**
+ * @author soulChun
+ * @create 2018-12-20-15:11
+ */
+public class sConf {
+    public static final String USERNAME = "root";
+    public static final String PASSWORD = "root";
+    public static final String DRIVERNAME = "com.mysql.jdbc.Driver";
+    public static final String URL = "jdbc:mysql://localhost:3306/soul";
+}
